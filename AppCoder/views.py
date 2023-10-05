15d844d1abcec2798a1e983035610f5c0cfd45f9
Matias.L <matiasivanlombardo@gmail.com>
2023-10-05 20:23:59 -0300

    Pre-entrega3

---
+++ AppCoder/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def inicio(request):
+    return render(request, "AppCoder/index.html")
+
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+ 
+
+
